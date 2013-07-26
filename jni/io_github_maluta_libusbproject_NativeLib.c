392b087af82d232a28b6ade11dbd235f91481bc0
Tiago Maluta <tiago.maluta@gmail.com>
2013-07-26 16:42:37 -0300

    Return a list of vendor:product ids.
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- jni/io_github_maluta_libusbproject_NativeLib.c
+++ jni/io_github_maluta_libusbproject_NativeLib.c
@@ -23,10 +23,13 @@ JNIEXPORT jstring JNICALL Java_io_github_maluta_libusbproject_NativeLib_listDevi
 	libusb_device *dev;
 	struct libusb_device_descriptor desc;
 	int i = 0;
+	int j = 0;
 	libusb_device **devs;
 	int r;
 	ssize_t cnt;
 	int ret;
+
+	char usb_list[2000];
 
 	if (context == NULL) {
 		if (libusb_init(&context) == 0) {
@@ -41,56 +44,50 @@ JNIEXPORT jstring JNICALL Java_io_github_maluta_libusbproject_NativeLib_listDevi
 		int r = libusb_get_device_descriptor(dev, &desc);
 		if (r < 0) {
 			fprintf(stderr, "failed to get device descriptor");
-			//goto out4;
 		}
-		__android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", "%x:%x",desc.idVendor, desc.idProduct);
-		//if ((desc.idVendor == 0x0403) && (desc.idProduct == 0x6001))
-		//	break;
+
+		j += sprintf(usb_list+j,"%04x:%04x\n", desc.idVendor, desc.idProduct);
+
+		__android_log_print(ANDROID_LOG_DEBUG, "LOG_TAG", "%x:%x",
+				desc.idVendor, desc.idProduct);
+
 	}
 
-	/*	ret = libusb_open(dev, &device_handle);
-	 if (ret != 0) {
-	 //goto out4;
-	 }
+	return (*env)->NewStringUTF(env, usb_list);
 
-	 if (device_handle == NULL) {
-	 //goto out4;
-	 }*/
+	/* Some other functions that you can use */
 
-	/*switch (libusb_kernel_driver_active(device_handle, interface)) {
-	 case 0: {
-	 // nothing to do
-	 break;
-	 }
-	 case 1: {
-	 // driver active
-	 if (!libusb_detach_kernel_driver(device_handle, interface)) {
-	 // unable to detach interface
-	 libusb_close(device_handle);
-	 device_handle = NULL;
-	 //goto out3;
-	 }
-	 break;
-	 }
-	 default: {
-	 // error quering the device
-	 libusb_close(device_handle);
-	 device_handle = NULL;
-	 //goto out2;
-	 }
-	 }
+	//	ret = libusb_open(dev, &device_handle);
 
-	 // get interface
-	 if (libusb_claim_interface(device_handle, interface) != 0) {
-	 //goto out1;
-	 }*/
+	// if (device_handle == NULL) { }
 
-	/*libusb_control_transfer(device_handle,
-	 LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_VENDOR
-	 | LIBUSB_RECIPIENT_INTERFACE, 0, 0x0000, 0, 0, 0, 250);
-	 */
-	// libusb_bulk_transfer(device_handle, (0x02 | LIBUSB_ENDPOINT_OUT),relay/*&byte*/, 1, &actual, 1000);
-	//libusb_release_interface(device_handle, interface);
+	/*
+	switch (libusb_kernel_driver_active(device_handle, interface)) {
+	case 0: {
+		break;
+	}
+	case 1: {
 
-	return (*env)->NewStringUTF(env, "OK!");
+		if (!libusb_detach_kernel_driver(device_handle, interface)) {
+			// unable to detach interface
+			libusb_close(device_handle);
+			device_handle = NULL;
+			//goto out3;
+		}
+		break;
+	}
+	default: {
+		// error quering the device
+		libusb_close(device_handle);
+		device_handle = NULL;
+		//goto out2;
+	}
+	}*/
+
+	// get interface
+	// if (libusb_claim_interface(device_handle, interface) != 0) { }
+
+	// bulk transfer
+	// libusb_bulk_transfer(device_handle, (0x02 | LIBUSB_ENDPOINT_OUT), &byte, 1, &actual, 1000);
+    // libusb_release_interface(device_handle, interface);
 }
