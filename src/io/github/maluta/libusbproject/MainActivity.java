53da1fecb9b2c24eb4b5720ba2c2c2e771c370b8
Tiago Maluta <tiago.maluta@gmail.com>
2013-07-26 16:24:16 -0300

    Added widget to show usb list output
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- src/io/github/maluta/libusbproject/MainActivity.java
+++ src/io/github/maluta/libusbproject/MainActivity.java
@@ -5,19 +5,24 @@ import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ToggleButton;
 import io.github.maluta.libusbproject.R;
 
 public class MainActivity extends Activity {
 	private static final String TAG = null;
 	private NativeLib nativeLib = new NativeLib();
-	
+	private EditText t;
+	private String usb_devices;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		nativeLib.listDevices();
+		
+		usb_devices = nativeLib.listDevices();
+		t = (EditText) findViewById(R.id.editText1);
+		t.setText(usb_devices);
 
 	}
 
